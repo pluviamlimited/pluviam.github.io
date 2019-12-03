c886fec6624bc0f009d0e7a43c3f1a82f6856e05
pluviamlimited <55419380+pluviamlimited@users.noreply.github.com>
2019-12-03 18:00:15 -0500

    Create payment.php

---
+++ payment.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Street: <?php echo $_POST["street"]; ?><br>
+City: <?php echo $_POST["city"]; ?>
+State: <?php echo $_POST["state"]; ?>
+Postal Code: <?php echo $_POST["zip"]; ?>
+Country: <?php echo $_POST["country"]; ?>
+Email: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
